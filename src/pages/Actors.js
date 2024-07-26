498737bd752d8e4240c625477ce5ab30a435c5cc
Sundeep <sundeep.kaler@gmail.com>
2024-07-25 21:06:23 -0400

    complete lab

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState([])
+  
+  useEffect(() =>{
+    fetch(`http://localhost:4000/actors`)
+    .then(r => r.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error));
+  }, []);
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+      {<NavBar />}
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actors.map((actor) => {return (
+          <article>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie) => (<li>{movie}</li>))}
+            </ul>
+          </article>
+        )})}
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const [genres, setGenres] = useState([])
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() =>{
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then(r => r.json())
+    .then(data => {
+      setMovie(data)
+      setGenres(data.genres)
+    })
+    .catch(error => console.error(error));
+  }, [movieId]);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {genres.map((genre) => (<span>{genre}</span>))}
       </main>
     </>
   );
